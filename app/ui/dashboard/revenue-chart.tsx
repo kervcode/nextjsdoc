350826ab6cdfb0f05d75f1001235e4032285272b
Kervcodes <kervintz84@hotmail.com>
2025-01-18 21:01:26 -0500

    Commenting code out for troubleshooting

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -8,11 +8,13 @@ import { lusitana } from '@/app/ui/fonts';
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+export default async function RevenueChart(
+  // {
+  //   revenue,
+  // }: {
+  //   revenue: Revenue[];
+  // }
+) {
   const chartHeight = 350;
   // NOTE: Uncomment this code in Chapter 7
 
